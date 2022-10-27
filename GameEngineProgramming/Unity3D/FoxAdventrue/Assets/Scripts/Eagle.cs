02d61caa8d1228ab3ec4f2e758d518c97b7275c7
downkhg <downkhg@gmail.com>
2022-10-27 19:15:55 +0900

    상태구분없이 전환하기

--- GameEngineProgramming/Unity3D/FoxAdventrue/Assets/Scripts/Eagle.cs
+++ GameEngineProgramming/Unity3D/FoxAdventrue/Assets/Scripts/Eagle.cs
@@ -7,26 +7,22 @@ public class Eagle : MonoBehaviour
     public float Speed = 1;
     public float Site = 1;
 
-    public Vector3 vResponPoint;
+    public Transform trTargetPoint;
+    public Transform trResponPoint;
+    public Transform trPatrolPoint;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public bool isPatrol;
+    public bool isMove;
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(this.transform.position, Site);
 
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(trResponPoint.position, Time.deltaTime);
+
         Gizmos.color = Color.red;
-        Gizmos.DrawSphere(vResponPoint, Time.deltaTime);
+        Gizmos.DrawSphere(trPatrolPoint.position, Time.deltaTime);
     }
 
     private void FixedUpdate()
@@ -34,27 +30,71 @@ public class Eagle : MonoBehaviour
         Vector3 vPos = this.transform.position;
         Collider2D collider = Physics2D.OverlapCircle(vPos, Site, 1<<LayerMask.NameToLayer("Player"));
 
-        if(collider && collider.gameObject.tag == "Player")
+        if (isPatrol == false)
         {
-            Debug.Log("OverlapCircle:" + collider.gameObject.name);
-            Vector3 vTargetPos = collider.transform.position;
-            Vector3 vDist = vTargetPos - vPos;//위치의 차이를 이용한 거리구하기
-            Vector3 vDir = vDist.normalized;//두물체사이의 방향(평준화-거리를뺀 이동량)
-            float fDist = vDist.magnitude; //두물체사이의 거리(스칼라-순수이동량)
+            if (collider && collider.gameObject.tag == "Player")
+            {
+                Debug.Log("OverlapCircle:" + collider.gameObject.name);
+                MoveProcess(collider.transform.position);
+            }
+            else
+            {
+                //Vector3 vTargetPos = trResponPoint.position;
+                //Vector3 vDist = vTargetPos - vPos;//위치의 차이를 이용한 거리구하기
+                //Vector3 vDir = vDist.normalized;//두물체사이의 방향(평준화-거리를뺀 이동량)
+                //float fDist = vDist.magnitude; //두물체사이의 거리(스칼라-순수이동량)
 
-            if (fDist > Time.deltaTime)//한프레임의 이동거리보다 클때만 이동한다.
-                transform.position += vDir * Speed * Time.deltaTime;
+                //if (fDist > Time.deltaTime)//한프레임의 이동거리보다 클때만 이동한다.
+                //    transform.position += vDir * Speed * Time.deltaTime;
+                if (MoveProcess(trResponPoint.position) == false)
+                {
+                    trTargetPoint = trPatrolPoint;
+                    isPatrol = true;
+                }
+            }
         }
         else
         {
-            Vector3 vTargetPos = vResponPoint;
-            Vector3 vDist = vTargetPos - vPos;//위치의 차이를 이용한 거리구하기
-            Vector3 vDir = vDist.normalized;//두물체사이의 방향(평준화-거리를뺀 이동량)
-            float fDist = vDist.magnitude; //두물체사이의 거리(스칼라-순수이동량)
-
-            if (fDist > Time.deltaTime)//한프레임의 이동거리보다 클때만 이동한다.
-                transform.position += vDir * Speed * Time.deltaTime;
+            if (collider == null)
+                PatrolProcess();
+            else
+                isPatrol = false;
         }
+    }
+
+    public void PatrolProcess()
+    {
+        if (trTargetPoint)
+        {
+            bool isMove = MoveProcess(trTargetPoint.position);
+
+            if (trTargetPoint.gameObject.name == trResponPoint.gameObject.name)
+            {
+                if (isMove == false)
+                    trTargetPoint = trPatrolPoint;
+            }
+            else if (trTargetPoint.gameObject.name == trResponPoint.gameObject.name)
+            {
+                if (isMove == false)
+                    trTargetPoint = trPatrolPoint;
+            }
+        }
+    }
+
+    public bool MoveProcess(Vector3 vTargetPos)
+    {
+        Vector3 vPos = this.transform.position;
+        Vector3 vDist = vTargetPos - vPos;//위치의 차이를 이용한 거리구하기
+        Vector3 vDir = vDist.normalized;//두물체사이의 방향(평준화-거리를뺀 이동량)
+        float fDist = vDist.magnitude; //두물체사이의 거리(스칼라-순수이동량)
+
+        if (fDist > Time.deltaTime)//한프레임의 이동거리보다 클때만 이동한다.
+        {
+            transform.position += vDir * Speed * Time.deltaTime;
+            return true;
+        }
+        else
+            return false;
     }
 
     private void OnTriggerStay2D(Collider2D collision)
