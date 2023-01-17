94921d1e6678e926c04494307bdb5c04cde96707
downkhg <downkhg@gmail.com>
2023-01-17 17:13:24 +0900

    용어정리, 윈도우 플러그인

---
+++ GamePlatfromProgramming/WinFormsLibrary/WinfromPlugin.cs
@@ -0,0 +1,13 @@
+﻿//1.Windowsforms클래스라이브러리 생성
+//2..Net6(장기지원) 선택
+//3.Release 빌드 > 탭 > 상위폴더 > "bin/release/.Net6.0/dll"
+namespace WinFormsLibrary
+{
+    public class WinfromPlugin
+    {
+        static public void MessageBoxShow(string text, string caption)
+        {
+            MessageBox.Show(text, caption);
+        }
+    }
+}
